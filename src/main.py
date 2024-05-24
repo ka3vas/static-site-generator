154a6fa29d9355ad5df78b73f51282fbc65a911f
ka3vas <tyrnis@gmail.com>
2024-05-24 13:45:52 +0200

    added TextNode class

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from textnode import TextNode
+
+def main():
+    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(test)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        # Initializing the properties of the class
+        # self.text - The text content of the node
+        # self.text_type - The type of text this node contains, which is just a string like "bold" or "italic"
+        # self.url - The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        # Check if all properties are equal
+        return (
+            self == other.text and
+            self.text_type == other.text_type and 
+            self.url == other.url
+        )
+            
+        
+       
+        
+    def __repr__(self):
+        # Return a string representation of the TextNode object
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
